e3db68ab80e23392a636a06edf5b401b79c71a05
Николаев Андрей <agnikolaev@beeline.ru>
2023-06-22 19:41:22 +0300

    попытка 3

--- bash/run_init_db.sh
+++ bash/run_init_db.sh
@@ -4,7 +4,7 @@
 docker run --name sde-pg2 -p 5432:5432  -e POSTGRES_USER=test_sde -e POSTGRES_PASSWORD=@sde_password012 -e POSTGRES_DB=demo -e PGDATA=/var/lib/postgresql/data/pgdata -d -v C:/Users/AGNikolaev/Desktop/sde_test_db/sql:/var/lib/postgresql/data -v C:/Users/AGNikolaev/Desktop/sde_test_db/sql/init_db:/docker-entrypoint-initdb.d postgres
 
 # Даем время на внесение изменений
-sleep 60
+sleep 15
 
 #  Запускаем скрипт для заполнения БД
 docker exec sde-pg2 psql -U test_sde -d demo -f /var/lib/postgresql/data/init_db/demo.sql
